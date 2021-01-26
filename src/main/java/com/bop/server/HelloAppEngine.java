a0eb312ddeae11ac72988faec2986dabb58a3619
viktoria <ivanvikii@gmail.com>
2021-01-26 19:44:51 +0000

    set up project

---
+++ src/main/java/com/bop/server/HelloAppEngine.java
@@ -0,0 +1,35 @@
+package com.bop.server;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "bop.ai", value = "/hello")
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    Properties properties = System.getProperties();
+    
+    WelcomeMenu wm = new WelcomeMenu();
+
+    response.setContentType("text/plain");
+    response.getWriter().println("bop.ai ");
+
+  }
+
+  public static String getInfo() {
+    return "Version: " + System.getProperty("java.version")
+          + " OS: " + System.getProperty("os.name")
+          + " User: " + System.getProperty("user.name");
+  }
+
+}
---
+++ src/main/java/com/bop/server/WelcomeMenu.java
@@ -0,0 +1,18 @@
+package com.bop.server;
+
+public class WelcomeMenu {
+	
+	public WelcomeMenu(){
+	}
+	
+
+	public String printWelcome() {
+		// TODO Auto-generated method stub
+		String message;
+		message = "Welcome to the future of the music industry!";
+		return message;
+
+	}
+
+
+}
